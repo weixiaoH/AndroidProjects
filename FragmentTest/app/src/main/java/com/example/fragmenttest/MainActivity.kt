bc9c3a6ca684db3dd3a3903fd1aac7826d715880
weixiaohuang <306559369@qq.com>
2021-05-13 23:15:08 +0800

    shared

---
+++ FragmentTest/app/src/main/java/com/example/fragmenttest/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.fragmenttest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            replaceFragment(AnotherRightFragment())
+        }
+        replaceFragment(RightFragment())
+    }
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.rightLayout, fragment)
+        transaction.commit()
+    }
+}
