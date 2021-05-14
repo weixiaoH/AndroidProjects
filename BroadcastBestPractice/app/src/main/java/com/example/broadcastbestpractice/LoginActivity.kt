9fe153bae01aee5e938581137993799ed0df6c98
weixiaohuang <306559369@qq.com>
2021-05-14 20:56:40 +0800

    Broadcast

--- BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/LoginActivity.kt
+++ BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.broadcastbestpractice
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -13,19 +14,36 @@ class LoginActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val prefs = getPreferences(Context.MODE_PRIVATE)
+        val isRemember = prefs.getBoolean("remember_password", false)
+        if (isRemember) {
+            // 将账号和密码都设置到文本框中
+            val account = prefs.getString("account", "")
+            val password = prefs.getString("password", "")
+            binding.accountEdit.setText(account)
+            binding.passwordEdit.setText(password)
+            binding.rememberPass.isChecked = true
+        }
         binding.login.setOnClickListener {
             val account = binding.accountEdit.text.toString()
             val password = binding.passwordEdit.text.toString()
             // 如果账号是admin且密码是123456，就认为登录成功
             if (account == "admin" && password == "123456") {
+                val editor = prefs.edit()
+                if (binding.rememberPass.isChecked) { // 检查复选框是否被选中
+                    editor.putBoolean("remember_password", true)
+                    editor.putString("account", account)
+                    editor.putString("password", password)
+                } else {
+                    editor.clear()
+                }
+                editor.apply()
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
                 finish()
             } else {
-                Toast.makeText(
-                    this, "account or password is invalid",
-                    Toast.LENGTH_SHORT
-                ).show()
+                Toast.makeText(this, "account or password is invalid",
+                    Toast.LENGTH_SHORT).show()
             }
         }
     }
