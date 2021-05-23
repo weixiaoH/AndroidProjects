b027ff05ee07a75fabd69f205b684b8aa356d537
weixiaohuang <306559369@qq.com>
2021-05-24 01:27:41 +0800

    material

---
+++ MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.kt
@@ -0,0 +1,103 @@
+package com.example.materialtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.materialtest.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    val fruits = mutableListOf(Fruit("Apple", R.drawable.apple), Fruit("Banana",
+        R.drawable.banana), Fruit("Orange", R.drawable.orange), Fruit("Watermelon",
+        R.drawable.watermelon), Fruit("Pear", R.drawable.pear), Fruit("Grape",
+        R.drawable.grape), Fruit("Pineapple", R.drawable.pineapple), Fruit("Strawberry",
+        R.drawable.strawberry), Fruit("Cherry", R.drawable.cherry), Fruit("Mango",
+        R.drawable.mango))
+    val fruitList = ArrayList<Fruit>()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        //recycleView
+        initFruits()
+        val layoutManager = GridLayoutManager(this, 2)
+        binding.recyclerView.layoutManager = layoutManager
+        val adapter = FruitAdapter(this, fruitList)
+        binding.recyclerView.adapter = adapter
+
+        binding.swipeRefresh.setColorSchemeResources(R.color.colorPrimary)
+        binding.swipeRefresh.setOnRefreshListener {
+            refreshFruits(adapter)
+        }
+
+
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
+
+        binding.navView.setCheckedItem(R.id.navCall)
+        binding.navView.setNavigationItemSelectedListener {
+            binding.drawerLayout.closeDrawers()
+            true
+        }
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Data deleted", Snackbar.LENGTH_SHORT)
+                .setAction("Undo") {
+                    Toast.makeText(this, "Data restored", Toast.LENGTH_SHORT).show()
+                }
+                .show()
+        }
+
+
+    }
+
+    private fun refreshFruits(adapter: FruitAdapter) {
+        thread {
+            Thread.sleep(2000)
+            runOnUiThread {
+                initFruits()
+                adapter.notifyDataSetChanged()
+                binding.swipeRefresh.isRefreshing = false
+            }
+        }
+    }
+
+    private fun initFruits() {
+        fruitList.clear()
+        repeat(50) {
+            val index = (0 until fruits.size).random()
+            fruitList.add(fruits[index])
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> binding.drawerLayout.openDrawer(GravityCompat.START)
+            R.id.backup -> Toast.makeText(this, "You clicked Backup",
+                Toast.LENGTH_SHORT).show()
+                    R.id.delete -> Toast.makeText(this, "You clicked Delete",
+                Toast.LENGTH_SHORT).show()
+            R.id.settings -> Toast.makeText(this, "You clicked Settings",
+                Toast.LENGTH_SHORT).show()
+        }
+        return true
+    }
+}
