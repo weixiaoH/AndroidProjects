ce6d6459972275802aaa0ff2974f410e5f0ab931
huangwx <huangwx@trip.com>
2021-05-27 16:32:02 +0800

    Jetpack

--- JetpackTest/app/src/main/java/com/example/jetpacktest/MainActivity.kt
+++ JetpackTest/app/src/main/java/com/example/jetpacktest/MainActivity.kt
@@ -6,8 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.core.content.edit
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.jetpacktest.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     lateinit var viewModel: MainViewModel
@@ -25,20 +27,57 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this, MainViewModelFactory(countReserved))
                 .get(MainViewModel::class.java)
         binding.plusOneBtn.setOnClickListener {
-            viewModel.counter++
-            refreshCounter()
+            viewModel.plusOne()
         }
         binding.clearBtn.setOnClickListener {
-            viewModel.counter = 0
-            refreshCounter()
+            viewModel.clear()
         }
+        viewModel.counter.observe(this, Observer { count ->
+            binding.infoText.text = count.toString()
+        })
+        binding.getUserBtn.setOnClickListener {
+            val userId = (0..10000).random().toString()
+            viewModel.getUser(userId)
+        }
+        viewModel.user.observe(this, Observer { user ->
+            binding.infoText.text = user.firstName
+        })
         refreshCounter()
+
+
+        val userDao = AppDatabase.getDatabase(this).userDao()
+        val user1 = User("Tom", "Brady", 40)
+        val user2 = User("Tom", "Hanks", 63)
+        binding.addDataBtn.setOnClickListener {
+            thread {
+                user1.id = userDao.insertUser(user1)
+                user2.id = userDao.insertUser(user2)
+            }
+        }
+        binding.updateDataBtn.setOnClickListener {
+            thread {
+                user1.age = 42
+                userDao.updateUser(user1)
+            }
+        }
+        binding.deleteDataBtn.setOnClickListener {
+            thread {
+                userDao.deleteUserByLastName("Hanks")
+            }
+        }
+        binding.queryDataBtn.setOnClickListener {
+            thread {
+                for (user in userDao.loadAllUsers()) {
+                    Log.d("MainActivity", user.toString())
+                }
+            }
+        }
     }
 
     override fun onPause() {
         super.onPause()
         sp.edit {
-            putInt("count_reserved", viewModel.counter)
+            putInt("count_reserved", viewModel.counter.value ?: 0)
         }
     }
 
--- JetpackTest/app/src/main/java/com/example/jetpacktest/MainViewModel.kt
+++ JetpackTest/app/src/main/java/com/example/jetpacktest/MainViewModel.kt
@@ -1,18 +1,34 @@
 package com.example.jetpacktest
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class MainViewModel(countReserved: Int) : ViewModel() {
-    val counter = MutableLiveData<Int>()
+    val counter: LiveData<Int>
+        get() = _counter
+
+    private val _counter = MutableLiveData<Int>()
+
+    private val userIdLiveData = MutableLiveData<String>()
+
+    val user: LiveData<User> = Transformations.switchMap(userIdLiveData) { userId ->
+        Repository.getUser(userId)
+    }
+
     init {
-        counter.value = countReserved
+        _counter.value = countReserved
     }
     fun plusOne() {
-        val count = counter.value ?: 0
-        counter.value = count + 1
+        val count = _counter.value ?: 0
+        _counter.value = count + 1
     }
     fun clear() {
-        counter.value = 0
+        _counter.value = 0
+    }
+
+    fun getUser(userId: String) {
+        userIdLiveData.value = userId
     }
 }
