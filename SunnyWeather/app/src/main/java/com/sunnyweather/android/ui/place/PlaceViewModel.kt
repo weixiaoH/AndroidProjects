9ada6050ab4f60e6295d8ca76da1892db8169dcc
weixiaohuang <306559369@qq.com>
2021-05-30 00:14:48 +0800

    加入显示天气信息的功能

--- SunnyWeather/app/src/main/java/com/sunnyweather/android/ui/place/PlaceViewModel.kt
+++ SunnyWeather/app/src/main/java/com/sunnyweather/android/ui/place/PlaceViewModel.kt
@@ -7,6 +7,11 @@ import com.sunnyweather.android.logic.Repository
 import com.sunnyweather.android.logic.model.Place
 
 class PlaceViewModel : ViewModel() {
+
+    fun savePlace(place: Place) = Repository.savePlace(place)
+    fun getSavedPlace() = Repository.getSavedPlace()
+    fun isPlaceSaved() = Repository.isPlaceSaved()
+
     private val searchLiveData = MutableLiveData<String>()
     val placeList = ArrayList<Place>()
     val placeLiveData = Transformations.switchMap(searchLiveData) { query ->
